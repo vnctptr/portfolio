bb0439899bcc5d57a0e9759ef7d749ada5ca3526
Vincent Potrykus <vapotrykus@gmail.com>
2024-01-04 14:38:13 -0800

    Add project cards

---
+++ app/components/Card.tsx
@@ -0,0 +1,8 @@
+export default function Card() {
+    return (
+        <div>
+            <h1>Project Name</h1>
+            <img src="https://picsum.photos/200/300" alt="random image" />
+        </div>
+    )
+}
