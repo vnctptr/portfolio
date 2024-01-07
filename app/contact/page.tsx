6415619d58778c8239bf93b9618c0ac273915cd7
Vincent Potrykus <vapotrykus@gmail.com>
2024-01-06 17:34:48 -0800

    Remove unused variables and imports

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,8 @@
 import Navbar from "@/app/components/Navbar/Navbar";
-import Card from "@/app/components/Card/Card";
 import React from "react";
 import "./page.css";
 
 export default function Page() {
-  let habitImageSrc = "/habit-iphone.png";
-  let devJournalImageSrc = "/journal-mac.png";
   return (
     <>
       <Navbar></Navbar>
