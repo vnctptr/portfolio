508cf4636dafe05fd28a623a256c8154d08ebea6
Vincent Potrykus <vapotrykus@gmail.com>
2024-01-08 15:34:27 -0800

    Add about section

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,17 @@ export default function Home() {
   return (
     <>
       <Navbar></Navbar>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+      <main className="flex flex-col items-center justify-between">
+        <div className="grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-2 lg:text-left">
+          <div className="col-span-2 mx-8">
+            Hi! I'm a software developer based in Victoria, BC. I'm currently
+            working at a startup. I'm passionate about building software that
+            improves people's lives. I'm always looking for new opportunities to
+            learn and grow.
+          </div>
+        </div>
+
         <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-2 lg:text-left">
           <Card imageSrc={habitImageSrc} />
           <Card imageSrc={devJournalImageSrc} />
