805ba6eedf23a1b27bb407f2ee341ca1a7a906d0
Vincent Potrykus <vapotrykus@gmail.com>
2024-01-14 20:16:07 -0800

    Rework resume download button

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,6 +1,9 @@
+import "./Navbar.css";
+import Image from "next/image";
+import React from "react";
 export default function Navbar() {
   return (
-    <nav className="flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 sm:items-baseline w-full">
+    <nav className="flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-6 px-6 sm:items-baseline w-full">
       <div className="mb-2 sm:mb-0">
         <a
           href="/"
@@ -8,12 +11,23 @@ export default function Navbar() {
         ></a>
       </div>
       <div>
-        <a
-          href="contact"
-          className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2 header-font"
-        >
-          Contact
-        </a>
+        <div className="download-button px-5 py-1.5 rounded-3xl m-2">
+          <a
+            href="/resume.pdf"
+            download="vincent-potrykus-resume.pdf"
+            className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2 header-font download-button"
+          >
+            Resume
+            <Image
+              className="icon pl-2"
+              src="/download.svg"
+              alt="GitHub"
+              width={30}
+              height={30}
+              priority
+            />
+          </a>
+        </div>
       </div>
     </nav>
   );
