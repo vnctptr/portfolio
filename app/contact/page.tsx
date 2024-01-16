70b212523fc0ed17858d3220224700abe6eaa652
Vincent Potrykus <vapotrykus@gmail.com>
2024-01-15 19:50:17 -0800

    Refactor email link in contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/app/components/Navbar/Navbar";
 import React from "react";
 import "./page.css";
 import SocialIcons from "@/app/components/SocialIcons/SocialIcons";
+import { EMAIL_LINK } from "@/app/constants";
 
 export default function Page() {
   return (
@@ -13,11 +14,8 @@ export default function Page() {
         </div>
         <p className="mt-6 mb-8 link-accent">
           If you&apos;d like to get in touch with me, feel free to send me an
-          email at{" "}
-          <a href="mailto:vincentpotrykus@gmail.com">
-            vincentpotrykus@gmail.com
-          </a>
-          . You can also find me on Github and LinkedIn using the icons below.
+          email at <a href={EMAIL_LINK}>vincentpotrykus@gmail.com</a>. You can
+          also find me on Github and LinkedIn using the icons below.
         </p>
         <SocialIcons />
       </div>
