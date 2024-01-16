c38accdc0b25de386bf14d39f1aaaa71bc7b0a4a
Vincent Potrykus <vapotrykus@gmail.com>
2024-01-15 19:59:54 -0800

    Fix margin on contact page content

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,16 +8,18 @@ export default function Page() {
   return (
     <>
       <Navbar />
-      <div className="contact-inner-container flat-box">
-        <div className="text-center">
-          <h1 className="text-2xl">Say Hi!</h1>
+      <div className="contact-inner-container">
+        <div className="flat-box mx-5">
+          <div className="text-center">
+            <h1 className="text-2xl">Say Hi!</h1>
+          </div>
+          <p className="mt-6 mb-8 link-accent">
+            If you&apos;d like to get in touch with me, feel free to send me an
+            email at <a href={EMAIL_LINK}>vincentpotrykus@gmail.com</a>. You can
+            also find me on Github and LinkedIn using the icons below.
+          </p>
+          <SocialIcons />
         </div>
-        <p className="mt-6 mb-8 link-accent">
-          If you&apos;d like to get in touch with me, feel free to send me an
-          email at <a href={EMAIL_LINK}>vincentpotrykus@gmail.com</a>. You can
-          also find me on Github and LinkedIn using the icons below.
-        </p>
-        <SocialIcons />
       </div>
     </>
   );
