1da0d6eb38d55b05f2213bcc1de1d1a1127ca125
Vincent Potrykus <vapotrykus@gmail.com>
2024-01-18 14:07:15 -0800

    Add title to project card and refactor the top bar

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -4,14 +4,23 @@ import React from "react";
 import "./Card.css";
 import Image from "next/image";
 
-export default function Card({ imageSrc }: { imageSrc: string }) {
+export default function Card({
+  imageSrc,
+  title,
+}: {
+  imageSrc: string;
+  title: string;
+}) {
   return (
     <div className="m-8">
       <div className="card-container">
         <div className="card-top-bar">
-          {[...Array(3)].map((_, index) => (
-            <div key={index} className="small-circle" />
-          ))}
+          <div className="flex items-center">
+            {[...Array(3)].map((_, index) => (
+              <div key={index} className="small-circle" />
+            ))}
+            <p className="pl-2 header-font">{title}</p>
+          </div>
         </div>
         <div className="card-content">
           <Image
